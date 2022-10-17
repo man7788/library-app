83752ae718eaf8bcbe7fe4d429e94afa1603aaea
man7788 <jerman1841@gmail.com>
2022-10-18 01:44:05 +0800

    Update save and display array, add two buttons

--- javascript.js
+++ javascript.js
@@ -15,30 +15,76 @@ Book.prototype.info = function() {
 let addButton = document.querySelector('.add-button');
 addButton.addEventListener('click', addBookToLibrary);
 
-function addBookToLibrary() {
-  const userTitle = document.querySelector('#title').value;
-  const userAuthor = document.querySelector('#author').value;
-  const userPages = document.querySelector('#pages').value;
-  const userRead = document.querySelector('#read').value;
-  let bookObj = new Book(userTitle, userAuthor, userPages, userRead);
-  myLibrary.push(bookObj);
-  displayInfo();
-}
-
-
-const cardTitle = document.querySelector('.card div:nth-child(1)');
-const cardAuthor = document.querySelector('.card div:nth-child(2)');
-const cardPages = document.querySelector('.card div:nth-child(3)');
-const cardRead = document.querySelector('.card div:nth-child(4)');
-
-function displayInfo() {
-  let book;
-  for(i = 0;i < myLibrary.length;i++) {
-    book = myLibrary[i];
+let newBookButton = document.querySelector('.new-button');
+newBookButton.addEventListener('click', () => {
+  if (document.querySelector('form').style.display === 'none') {
+    document.querySelector('form').style.display = 'block';
   }
-  cardTitle.textContent += book.title;
-  cardAuthor.textContent += book.author;
-  cardPages.textContent += book.pages;
-  cardRead.textContent += book.read;
+})
+
+function addBookToLibrary() {
+  const inputTitle = document.querySelector('#title').value;
+  const inputAuthor = document.querySelector('#author').value;
+  const inputPages = document.querySelector('#pages').value;
+  const inputRead = document.querySelector('#read').value;
+  let bookObj = new Book(inputTitle, inputAuthor, inputPages, inputRead);
+  myLibrary.push(bookObj);
+  document.querySelector('.card-container').replaceChildren();
+  loopBookList();
 }
 
+function loopBookList() {
+  for (i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i] === undefined) { 
+      continue; 
+    } else {
+    let bookTitle = myLibrary[i].title;
+    let bookAuthor = myLibrary[i].author;
+    let bookPages = myLibrary[i].pages;
+    let bookRead = myLibrary[i].read;
+    addCard(bookTitle, bookAuthor, bookPages, bookRead, i);
+    }
+  }
+}
+
+function addCard(title, author, pages, read, attribute) {
+  const libraryCard = document.createElement('div');
+  const cardTitle = document.createElement('div');
+  const cardAuthor = document.createElement('div');
+  const cardPages = document.createElement('div');
+  const cardRead = document.createElement('div');
+  const removeButton = document.createElement('button');
+  const changeButton = document.createElement('button');
+
+  cardTitle.textContent = 'Title: ' + title;
+  cardAuthor.textContent = 'Author: ' + author;
+  cardPages.textContent = 'Pages: ' + pages;
+  cardRead.textContent = 'Read: ' + read;
+  removeButton.textContent = 'Remove';
+  removeButton.setAttribute('id', attribute);
+  removeButton.addEventListener('click', (e) => {
+    myLibrary[e.target.id] = undefined;
+    document.querySelector('.card-container').replaceChildren();
+    loopBookList();
+  })
+  changeButton.textContent = 'Read Status';
+  changeButton.setAttribute('id', attribute);
+  changeButton.addEventListener('click', (e) => {
+    if (myLibrary[e.target.id].read === 'yes') {
+      myLibrary[e.target.id].read = 'no';
+    } else if (myLibrary[e.target.id].read === 'no') {
+      myLibrary[e.target.id].read = 'yes';
+    }
+    document.querySelector('.card-container').replaceChildren();
+    loopBookList();
+  })
+
+  libraryCard.classList.add('card');
+  libraryCard.appendChild(cardTitle);
+  libraryCard.appendChild(cardAuthor);
+  libraryCard.appendChild(cardPages);
+  libraryCard.appendChild(cardRead);
+  libraryCard.appendChild(removeButton);
+  libraryCard.appendChild(changeButton);
+  document.querySelector('.card-container').appendChild(libraryCard);
+}
